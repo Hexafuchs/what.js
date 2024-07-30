88baacf30e5184862cf2e123294a1e770ae046dd
Leo <leo@hexafuchs.de>
2024-07-30 15:16:10 +0200

    :white_check_mark: Add tests for isString

--- test/types/string.test.ts
+++ test/types/string.test.ts
@@ -1,7 +1,9 @@
-import { describe, expect, it } from "vitest";
-import {isString} from "../../src";
+import { describe } from 'vitest';
+import { testAssertIs, testAssertIsNot, testAssertIsOrNull } from '../helper/assertions';
+import { isString, assertIsNotString, assertIsString, assertIsStringOrNull } from '../../src';
+import { testIs } from '../helper/checker';
 
-const tests = [
+const baseTests: Array<[string, unknown, boolean]> = [
   ["null", null, false],
   ["undefined", undefined, false],
   ["true", true, false],
@@ -14,17 +16,29 @@ const tests = [
   ["zero float", 0.0, false],
   ["positive float", 42.0, false],
   ["negative float", -42.0, false],
+  ["zero bigint", BigInt(0), false],
+  ["big bigint", BigInt(9007199254740991), false],
   ["functions", () => {}, false],
   ["empty arrays", [], false],
   ["non-empty arrays", [42], false],
   ["empty objects", {}, false],
   ["non-empty objects", {'hello': 'world'}, false],
-]
+  ["empty sets", new Set, false],
+  ["non-empty sets", new Set([4,4,2]), false],
+  ["empty map", new Map, false],
+  ["non-empty map", new Map([[1, 'hello'], [2, 'world']]), false],
+  ["regexp", /helloworld/, false],
+  ["promise", (async () => {})(), false],
+  ["error", new Error, false],
+  ["date", new Date, false],
+];
 
-describe("isString", () => {
-  for (const test of tests) {
-    it("should be " + (test[2] ? 'positive' : 'negative') + " about " + test[0], () => {
-      expect(isString(test[1])).toBe(test[2]);
-    });
-  }
+describe("string checker", () => {
+  testIs(isString, baseTests);
+});
+
+describe("string assertions", () => {
+  testAssertIs(assertIsString, baseTests);
+  testAssertIsNot(assertIsNotString, baseTests);
+  testAssertIsOrNull(assertIsStringOrNull, baseTests);
 });
