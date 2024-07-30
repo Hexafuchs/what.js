56dbdd159a8e182eddbd9077b32e00f2ca81521e
Leo <leo@hexafuchs.de>
2024-07-30 15:16:46 +0200

    :white_check_mark: Add tests for isNumber, isBigInt and isNumericPrimitive

--- test/types/number.test.ts
+++ test/types/number.test.ts
@@ -1,8 +1,17 @@
-import { describe, expect, it } from "vitest";
-import { isNumber } from "../../src";
+import { describe } from "vitest";
+import {
+  assertIsBigInt, assertIsBigIntOrNull,
+  assertIsNotBigInt,
+  assertIsNotNumber, assertIsNotNumericPrimitive,
+  assertIsNumber,
+  assertIsNumberOrNull, assertIsNumericPrimitive, assertIsNumericPrimitiveOrNull,
+  isBigInt,
+  isNumber, isNumericPrimitive
+} from "../../src";
+import { testIs } from '../helper/checker';
+import { testAssertIs, testAssertIsNot, testAssertIsOrNull } from '../helper/assertions';
 
-const tests = [
-  ["null", null, false],
+const numberTests: Array<[string, unknown, boolean]> = [
   ["undefined", undefined, false],
   ["true", true, false],
   ["false", false, false],
@@ -14,6 +23,8 @@ const tests = [
   ["zero float", 0.0, true],
   ["positive float", 42.0, true],
   ["negative float", -42.0, true],
+  ["zero bigint", BigInt(0), false],
+  ["big bigint", BigInt(9007199254740991), false],
   ["functions", () => {}, false],
   ["empty arrays", [], false],
   ["non-empty arrays", [42], false],
@@ -21,10 +32,84 @@ const tests = [
   ["non-empty objects", {'hello': 'world'}, false],
 ]
 
-describe("isNumber", () => {
-  for (const test of tests) {
-    it("should be " + (test[2] ? 'positive' : 'negative') + " about " + test[0], () => {
-      expect(isNumber(test[1])).toBe(test[2]);
-    });
-  }
+describe("number checker", () => {
+  testIs(isNumber, numberTests);
+});
+
+describe("number assertions", () => {
+  testAssertIs(assertIsNumber, numberTests);
+  testAssertIsNot(assertIsNotNumber, numberTests);
+  testAssertIsOrNull(assertIsNumberOrNull, numberTests);
+});
+
+
+const bigintTests: Array<[string, unknown, boolean]> = [
+  ["undefined", undefined, false],
+  ["true", true, false],
+  ["false", false, false],
+  ["empty string", "", false],
+  ["non-empty string", "hello", false],
+  ["zero", 0, false],
+  ["positive integer", 42, false],
+  ["negative integer", -42, false],
+  ["zero float", 0.0, false],
+  ["positive float", 42.0, false],
+  ["negative float", -42.0, false],
+  ["zero bigint", BigInt(0), true],
+  ["big bigint", BigInt(9007199254740991), true],
+  ["functions", () => {}, false],
+  ["empty arrays", [], false],
+  ["non-empty arrays", [42], false],
+  ["empty objects", {}, false],
+  ["non-empty objects", {'hello': 'world'}, false],
+]
+
+describe("bigint checker", () => {
+  testIs(isBigInt, bigintTests);
+});
+
+describe("bigint assertions", () => {
+  testAssertIs(assertIsBigInt, bigintTests);
+  testAssertIsNot(assertIsNotBigInt, bigintTests);
+  testAssertIsOrNull(assertIsBigIntOrNull, bigintTests);
+});
+
+
+const numericTests: Array<[string, unknown, boolean]> = [
+  ["undefined", undefined, false],
+  ["true", true, false],
+  ["false", false, false],
+  ["empty string", "", false],
+  ["non-empty string", "hello", false],
+  ["zero", 0, true],
+  ["positive integer", 42, true],
+  ["negative integer", -42, true],
+  ["zero float", 0.0, true],
+  ["positive float", 42.0, true],
+  ["negative float", -42.0, true],
+  ["zero bigint", BigInt(0), true],
+  ["big bigint", BigInt(9007199254740991), true],
+  ["functions", () => {}, false],
+  ["empty arrays", [], false],
+  ["non-empty arrays", [42], false],
+  ["empty objects", {}, false],
+  ["non-empty objects", {'hello': 'world'}, false],
+  ["empty sets", new Set, false],
+  ["non-empty sets", new Set([4,4,2]), false],
+  ["empty map", new Map, false],
+  ["non-empty map", new Map([[1, 'hello'], [2, 'world']]), false],
+  ["regexp", /helloworld/, false],
+  ["promise", (async () => {})(), false],
+  ["error", new Error, false],
+  ["date", new Date, false],
+]
+
+describe("numeric primitive checker", () => {
+  testIs(isNumericPrimitive, numericTests);
+});
+
+describe("numeric primitive assertions", () => {
+  testAssertIs(assertIsNumericPrimitive, numericTests);
+  testAssertIsNot(assertIsNotNumericPrimitive, numericTests);
+  testAssertIsOrNull(assertIsNumericPrimitiveOrNull, numericTests);
 });
